4b7abeadda57d213c2c0c4eb37bc92c49550bef6
K-Siddharth06 <ksiddharth2006@gmail.com>
2026-07-29 19:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.49%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,11 +1,33 @@
 class Solution(object):
     def sortColors(self, nums):
+        zero = 0
+        one = 0
+        two = 0
 
-        n = len(nums)
+        # Count the occurrences
+        for num in nums:
+            if num == 0:
+                zero += 1
+            elif num == 1:
+                one += 1
+            else:
+                two += 1
 
-        for i in range(n):
-            for j in range(n - i - 1):
-                if nums[j] > nums[j + 1]:
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        # Fill the array with 0s
+        i = 0
+        while zero > 0:
+            nums[i] = 0
+            i += 1
+            zero -= 1
 
-        return nums
+        # Fill the array with 1s
+        while one > 0:
+            nums[i] = 1
+            i += 1
+            one -= 1
+
+        # Fill the array with 2s
+        while two > 0:
+            nums[i] = 2
+            i += 1
+            two -= 1
